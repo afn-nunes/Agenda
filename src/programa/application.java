72104090492ccd8e244f863b458f82e5f0186521
afnnunes <afn.nunes@gmail.com>
2021-07-07 22:06:31 -0300

    Criação da classe pessoa

--- src/programa/application.java
+++ src/programa/application.java
@@ -1,5 +1,15 @@
 package programa;
 
+/*Faça um programa em Java para simular uma parte do funcionamento de
+uma agenda de contatos. 
+Você deverá ter as opções de incluir uma pessoa e listar todas as pessoas cadastradas. 
+Cada pessoa terá nome, sobrenome e telefone de contato. 
+O número máximo de contatos que podem ser salvos é
+definido por você. Fique à vontade.
+Use o código fornecido na plataforma de aprendizagem para começar o seu
+projeto.
+Após finalizar o programa, compartilhe-o em sua conta do GitHub./ */
+
 import java.util.Scanner;
 
 public class application {
@@ -7,7 +17,7 @@ public class application {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int opcao;
-
+  
     do {
       System.out.println("Escolha uma opção:");
       System.out.println("1 - Incluir pessoa");
